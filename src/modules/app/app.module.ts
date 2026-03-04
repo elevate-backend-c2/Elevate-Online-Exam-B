396cf95a00d19334a012009f35a188f216756cd6
Yousef emad mikhail <mad33833@gmail.com>
2026-03-04 21:34:39 +0200

    add config module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const envFilePath =
+  process.env.NODE_ENV === 'production'
+    ? 'config/.env.production'
+    : process.env.NODE_ENV === 'stagging'
+      ? 'config/.env.stagging'
+      : 'config/.env.development';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
