69856dcfccfd1778463de014b50b7c899441971f
Yousef emad mikhail <mad33833@gmail.com>
2026-03-04 22:01:40 +0200

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,10 +20,17 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('api');
-
+  const config = new DocumentBuilder()
+    .setTitle('Exam Platform API')
+    .setDescription('The Exam Platform API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+  Logger.log(`API docs: /api/docs`);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   await app.listen(port);
 }
-bootstrap();
+void bootstrap();
