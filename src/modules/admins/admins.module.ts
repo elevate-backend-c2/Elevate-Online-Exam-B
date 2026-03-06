4b68fccc3ddc0bcb7b769e740a7b62cc9d1f4005
‫Mohannad Shazli <mohannadshazli@gmail.com>
2026-03-06 21:46:11 +0200

    add super admin seed service to create super admin on startup

--- src/modules/admins/admins.module.ts
+++ src/modules/admins/admins.module.ts
@@ -3,11 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AdminsController } from './admins.controller';
 import { AdminsService } from './admins.service';
 import { Admin, AdminSchema } from './schemas/admin.schema';
+import { SuperAdminSeedService } from './admins.seed';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
+  imports: [
+    //User module should export the user model and import it here to use in seeding super admin
+    //UserModule,
+    MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+  ],
   controllers: [AdminsController],
-  providers: [AdminsService],
+  providers: [AdminsService, SuperAdminSeedService],
   exports: [MongooseModule],
 })
 export class AdminsModule {}
