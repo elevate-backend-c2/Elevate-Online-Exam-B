c5dd3a5a4d42b696877170399c279695b1ef900d
‫Mohannad Shazli <mohannadshazli@gmail.com>
2026-03-07 02:19:24 +0200

    feat(super-admin): add update admin permission endpoint and route handler of this endpoint

--- src/modules/admins/admins.controller.ts
+++ src/modules/admins/admins.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { AdminsService } from './admins.service';
 
 @Controller('admins')
-export class AdminsController {}
+export class AdminsController {
+  constructor(private readonly adminService: AdminsService) {}
+  @Post()
+  createAdmin() {
+    return this.adminService.createAdmin();
+  }
+
+  @Patch(':adminId/permissions')
+  updateAdminDiplomas(
+    @Param('adminId') adminId: string,
+    @Body('allowedDiplomas') allowedDiplomas: string[],
+  ) {
+    return this.adminService.updateAdminDiplomas(adminId, allowedDiplomas);
+  }
+
+  @Patch(':adminId/deactivate')
+  deactivateAdmin() {
+    return this.adminService.deactivateAdmin();
+  }
+}
--- src/modules/admins/admins.service.ts
+++ src/modules/admins/admins.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../users/schemas/user.schema';
+import { Model } from 'mongoose';
+import { Diploma } from '../diplomas/schemas/diploma.schema';
+// import { CreateAdminDto } from './dtos/create-admin.dto';
 
 @Injectable()
-export class AdminsService {}
+export class AdminsService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    @InjectModel(Diploma.name) private diplomaModel: Model<Diploma>,
+  ) {}
+  async createAdmin() {}
+  async updateAdminDiplomas(id: string, allowedDiplomas: string[]) {
+    const user = await this.userModel.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const diplomas = await this.diplomaModel
+      .find({
+        _id: { $in: allowedDiplomas },
+      })
+      .select('_id');
+
+    const foundIds = diplomas.map((d) => d._id.toString());
+
+    const invalidIds = allowedDiplomas.filter((id) => !foundIds.includes(id));
+
+    if (invalidIds.length > 0) {
+      throw new BadRequestException(
+        `Diplomas not found: ${invalidIds.join(', ')}`,
+      );
+    }
+
+    await this.userModel.updateOne(
+      { _id: id },
+      {
+        $addToSet: {
+          allowedDiplomas: { $each: allowedDiplomas },
+        },
+      },
+    );
+    return {
+      message: "Admin's diplomas updated successfully",
+    };
+  }
+  deactivateAdmin() {
+    throw new Error('Method not implemented.');
+  }
+}
